78d5431ae0072757a63b1f6ec27635de46852b67
beibao233 <beibao2333@gmail.com>
2022-05-28 15:06:03 +0800

    Fix Download Problem When Program Running At MacOS

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from System import operating_system
+import platform
 import asyncio
 import os
 
-if operating_system == "natives-windows":
+if platform.system() == 'Windows':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-download_loop = asyncio.new_event_loop()
+    download_loop = asyncio.new_event_loop()
+else:
+    download_loop = asyncio.get_event_loop()
+
 
 global base_path
 base_path = os.path.dirname(__file__)
